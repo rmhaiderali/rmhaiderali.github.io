1f5fdd346a83443acaeb07714950bfa550cc4c29
Haider Ali <rmha62@gmail.com>
2026-08-12 12:36:19 +0500

    Add NPM packages to homepage generation

--- src/generate-homepage.js
+++ src/generate-homepage.js
@@ -2,49 +2,92 @@ import fs from "fs"
 import mustache from "mustache"
 
 const user = "rmhaiderali"
-const userPage = user + ".github.io"
-const limit = "500"
-const reposTempFile = "temp/" + user + ".repos.json"
 
-const refresh = false
+const githubUser = user
+const githubRepoLimit = "500"
+const githubReposTemp = "temp/github.repos.by." + githubUser + ".json"
 
-let data = fs.existsSync(reposTempFile)
-  ? JSON.parse(fs.readFileSync(reposTempFile, "utf8"))
-  : null
+const npmUser = user
+const npmPackagesTemp = "temp/npm.packages.by." + npmUser + ".json"
 
-if (refresh || !data) {
+const stringify = (object) => JSON.stringify(object, null, 2)
+
+const uncached = []
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+let githubRepos = fs.existsSync(githubReposTemp)
+  ? JSON.parse(fs.readFileSync(githubReposTemp, "utf8"))
+  : uncached
+
+if (githubRepos === uncached) {
   const url =
-    "https://api.github.com/users/" + user + "/repos?per_page=" + limit
+    "https://api.github.com/users/" +
+    githubUser +
+    "/repos?per_page=" +
+    githubRepoLimit
 
   const res = await fetch(url)
 
-  data = await res.json()
-
-  if (!Array.isArray(data)) {
+  if (res.ok) {
+    githubRepos = await res.json()
+    fs.mkdirSync("temp", { recursive: true })
+    fs.writeFileSync(githubReposTemp, stringify(githubRepos))
+  } else {
     console.error("Error fetching data from GitHub API")
-    console.error(data)
-    process.exit(1)
+    console.error(await res.json())
   }
-
-  fs.mkdirSync("temp", { recursive: true })
-  fs.writeFileSync(reposTempFile, JSON.stringify(data, null, 2))
 }
 
-const pagesRepos = data
+const githubPages = githubRepos
   .filter((repo) => repo.has_pages)
-  .filter((repo) => repo.name !== userPage)
+  .filter((repo) => repo.name !== githubUser + ".github.io")
 
-if (pagesRepos.length === 0) {
-  console.log("No GitHub Pages found for user " + user)
-  process.exit(2)
+console.log(githubPages.length + " GitHub Pages found for user " + githubUser)
+
+if (githubPages.length)
+  console.log(githubPages.map((repo) => "• " + repo.name).join("\n"))
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+console.log()
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+let npmPackages = fs.existsSync(npmPackagesTemp)
+  ? JSON.parse(fs.readFileSync(npmPackagesTemp, "utf8"))
+  : uncached
+
+if (npmPackages === uncached) {
+  const url = "https://registry.npmjs.org/-/user/" + npmUser + "/package"
+
+  const res = await fetch(url)
+
+  if (res.ok) {
+    npmPackages = await res.json()
+    fs.mkdirSync("temp", { recursive: true })
+    fs.writeFileSync(npmPackagesTemp, stringify(npmPackages))
+  } else {
+    console.error("Error fetching data from NPM API")
+    console.error(await res.json())
+  }
 }
 
-console.log("Found " + pagesRepos.length + " GitHub Pages for user " + user)
-console.log(pagesRepos.map((repo) => repo.name).join("\n"))
+npmPackages = Object.keys(npmPackages)
+
+console.log(npmPackages.length + " NPM Packages found for user " + npmUser)
+
+if (npmPackages.length)
+  console.log(npmPackages.map((pkg) => "• " + pkg).join("\n"))
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 const template = fs.readFileSync("src/template.mustache", "utf8")
 
-const doc = mustache.render(template, { user, pagesRepos })
+const doc = mustache.render(template, {
+  npmUser,
+  githubUser,
+  npmPackages,
+  githubPages,
+})
 
 fs.mkdirSync("dist", { recursive: true })
 fs.writeFileSync("dist/index.html", doc)
